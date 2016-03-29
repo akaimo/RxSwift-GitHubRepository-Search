a57f9e8c55502c3253cfa8406893dd96134775ac
shuhei <kawashu0723@gmail.com>
2016-03-29 15:04:02 +0900

    add RxSwift

--- RxSwiftDemo/ViewController.swift
+++ RxSwiftDemo/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate {
+    @IBOutlet weak var tableView: UITableView!
+    let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView
+            .rx_setDelegate(self)
+            .addDisposableTo(disposeBag)
     }
 
     override func didReceiveMemoryWarning() {
