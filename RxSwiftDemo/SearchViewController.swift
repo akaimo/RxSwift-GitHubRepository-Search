1d029101748f7ee27074f7161ec9fecdcd842eb8
shuhei <kawashu0723@gmail.com>
2016-03-30 09:31:57 +0900

    Rxでフォームを制御

--- RxSwiftDemo/SearchViewController.swift
+++ RxSwiftDemo/SearchViewController.swift
@@ -11,11 +11,33 @@ import RxSwift
 import RxCocoa
 
 class SearchViewController: UIViewController {
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var validMessage: UILabel!
+    
     let viewModel = SearchViewModel()
     let disposeBag = DisposeBag()
+    let minimumSize = 3
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "GitHub リポジトリ検索"
+        
+        let searchValid = searchTextField.rx_text
+            .map { [weak self] in $0.characters.count >= self?.minimumSize }
+            .shareReplay(1)
+        
+        searchValid
+            .bindTo(validMessage.rx_hidden)
+            .addDisposableTo(disposeBag)
+        
+        searchValid
+            .bindTo(searchButton.rx_enabled)
+            .addDisposableTo(disposeBag)
+        
+        searchButton.rx_tap
+            .subscribeNext { print("tap") }
+            .addDisposableTo(disposeBag)
     }
     
     override func viewWillAppear(animated: Bool) {
